6848ee88e3378b7c75aec577db51d48ac83500d6
Marcos de Castro Borém <marcos.borem@hotmail.com>
2024-10-10 17:42:12 -0300

    feat(IdentityBlazor): added cookies and refactor cors

---
+++ Dima.Core/Requests/Account/LoginRequest.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Account;
+
+public class LoginRequest : Request
+{
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    public string Password { get; set; } = string.Empty;
+}
---
+++ Dima.Web/Pages/Identity/Login.razer.cs
@@ -0,0 +1,90 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Web.Security;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Identity;
+
+public partial class LoginPage : ComponentBase
+{
+    #region Services
+
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    [Inject]
+    public IAccountHandler Handler { get; set; } = null!;
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+
+    [Inject]
+    public ICookieAuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+
+    #endregion
+
+    #region Properties
+
+    public bool IsBusy { get; set; } = false;
+    public LoginRequest InputModel { get; set; } = new();
+
+    public MudForm? _mudForm;
+
+    #endregion
+
+    #region Overrides
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity is { IsAuthenticated: true })
+            NavigationManager.NavigateTo("/");
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var result = await Handler.LoginAsync(InputModel);
+
+            if (result.IsSuccess)
+            {
+                await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                AuthenticationStateProvider.NotifyAuthenticationStateChanged();
+                NavigationManager.NavigateTo("/");
+            }
+            else
+                Snackbar.Add(result.Message, Severity.Error);
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    // Propriedade que verifica se ambos os campos estão preenchidos
+    public bool CanSubmit => !string.IsNullOrWhiteSpace(InputModel.Email) && !string.IsNullOrWhiteSpace(InputModel.Password);
+
+    //public void SubmitForm()
+    //{
+    //    if (CanSubmit)
+    //    {
+    //        // Lógica para enviar o formulário ou fazer login
+    //        Console.WriteLine("Formulário enviado com sucesso!");
+    //    }
+    //}
+    #endregion
+}
